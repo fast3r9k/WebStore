f473d37cbc730b488a893b0795bc2a70bf4348dc
fast3r9k <faster20092009@yandex.ru>
2020-11-25 19:06:42 +0300

    Добавление списка сотрудников и метода More

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IConfiguration _Configuration;
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee {Id=1, LastName="Антонов",FirstName="Антон",Patronymic="Антонович",Age=20},
+            new Employee {Id=2, LastName="Андреев",FirstName="Андрей",Patronymic="Андреевич",Age=40},
+            new Employee {Id=3, LastName="Никитин",FirstName="Никита",Patronymic="Никитич",Age=30},
+        };
         public HomeController(IConfiguration Configuration) => _Configuration = Configuration;
 
         public IActionResult Index()
@@ -20,5 +27,17 @@ namespace WebStore.Controllers
         {
             return Content("Second action");
         }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees);
+        }   
+        
+        public IActionResult More(int id)
+        {
+
+            return View(__Employees.Where(e => e.Id == id).First());
+        }
+
     }
 }
