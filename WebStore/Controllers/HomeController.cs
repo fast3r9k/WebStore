8541671fd209685be8616284a780bce969507afd
fast3r9k <faster20092009@yandex.ru>
2020-11-29 12:02:17 +0300

    Add new actions

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -11,12 +12,7 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
         private readonly IConfiguration _Configuration;
-        private static readonly List<Employee> __Employees = new()
-        {
-            new Employee {Id=1, LastName="Антонов",FirstName="Антон",Patronymic="Антонович",Age=20},
-            new Employee {Id=2, LastName="Андреев",FirstName="Андрей",Patronymic="Андреевич",Age=40},
-            new Employee {Id=3, LastName="Никитин",FirstName="Никита",Patronymic="Никитич",Age=30},
-        };
+       
         public HomeController(IConfiguration Configuration) => _Configuration = Configuration;
 
         public IActionResult Index() => View();
@@ -25,16 +21,16 @@ namespace WebStore.Controllers
             return Content("Second action");
         }
 
-        public IActionResult Employees()
-        {
-            return View(__Employees);
-        }   
-        
-        public IActionResult More(int id)
-        {
 
-            return View(__Employees.Where(e => e.Id == id).First());
-        }
+
+        public IActionResult Blogs() => View();
+        public IActionResult BlogSingle() => View();
+        public IActionResult Cart() => View();
+        public IActionResult CheckOut() => View();
+        public IActionResult ContactUs() => View();
+        public IActionResult Login() => View();
+        public IActionResult ProductDetails() => View();
+        public IActionResult Shop() => View();
 
     }
 }
---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public class TestData
+    {
+        public static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Антонов", FirstName = "Антон", Patronymic = "Антонович", Age = 20 },
+            new Employee { Id = 2, LastName = "Андреев", FirstName = "Андрей", Patronymic = "Андреевич", Age = 40 },
+            new Employee { Id = 3, LastName = "Никитин", FirstName = "Никита", Patronymic = "Никитич", Age = 30 },
+        };
+    }
+}
