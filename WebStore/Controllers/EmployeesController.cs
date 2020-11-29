64917d56c52f38797dfe9b9497e188585b39ae48
fast3r9k <faster20092009@yandex.ru>
2020-11-29 12:01:34 +0300

    Employee controller

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private List<Employee> _Employees;
+        public EmployeesController()=> _Employees = TestData.__Employees;
+        public IActionResult Index() => View(_Employees);
+        public IActionResult More(int id)
+        {
+            var emp = TestData.__Employees.Where(e => e.Id == id).First();
+            if(emp is not null)
+                return View(emp);
+            return NotFound();
+
+        }
+    }
+}
