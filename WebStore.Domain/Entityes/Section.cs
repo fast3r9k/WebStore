e3c9690439cacb222a9b15a6320830a2d6395d7d
fast3r9k <faster20092009@yandex.ru>
2020-12-05 21:12:25 +0300

    Subject entities

---
+++ WebStore.Domain/Entityes/Section.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entityes.Base;
+using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.Domain.Entityes
+{
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public  int Order { get; set; }
+        public int? ParentId { get; set; }
+    }
+}
