e44d0dc986afe455ef077b29cb7be4f033349e60
fast3r9k <faster20092009@yandex.ru>
2020-12-05 21:04:09 +0300

    Base entity classes

---
+++ WebStore.Domain/Entityes/Base/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Entityes.Base.Interfaces;
+
+namespace WebStore.Domain.Entityes.Base
+{
+    public abstract class Entity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
