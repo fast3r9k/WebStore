1239c0e436ccb6e5778da9a57df7f99cbe2f837a
fast3r9k <faster20092009@yandex.ru>
2020-12-05 21:19:20 +0300

    Sections and brands in memory

---
+++ WebStore/Infrastructure/Interfaces/IProductData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entityes;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+        IEnumerable<Brand> GetBrands();
+    }
+}
