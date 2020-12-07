86f7175ffa39be848dd9be9db37f5cdd265260ca
fast3r9k <faster20092009@yandex.ru>
2020-12-07 11:06:21 +0300

    Catalog controller

--- WebStore/ViewModels/ProductViewModel.cs
+++ WebStore/ViewModels/ProductViewModel.cs
@@ -5,11 +5,10 @@ using WebStore.Domain.Entityes.Base.Interfaces;
 
 namespace WebStore.ViewModels
 {
-    public class ProductViewModel : INamedEntity, IOrderedEntity
+    public class ProductViewModel : INamedEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Order { get; set; }
         public  string ImageUrl { get; set; }
         public  decimal Price { get; set; }
     }
