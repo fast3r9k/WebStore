a3602a07e00ff4a5ff433fa8e262a809f8671ae0
fast3r9k <faster20092009@yandex.ru>
2020-12-09 22:07:20 +0300

    Add WebStore context

---
+++ WebStoreDAL/Context/WebStoreDb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entityes;
+
+namespace WebStore.DAL.Context
+{
+    class WebStoreDb : DbContext
+    {
+
+        public  DbSet<Product> Products { get; set; }
+
+        public  DbSet<Brand> Brands { get; set; }
+
+        public  DbSet<Section> Sections { get; set; }
+
+        public WebStoreDb(DbContextOptions options) : base(options){ }
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            base.OnModelCreating(model);
+        }
+    }
+}
