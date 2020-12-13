3ae164b8a8a16a3e0c124e7c62b8a93150adbe40
fast3r9k <faster20092009@yandex.ru>
2020-12-13 21:14:49 +0300

    Add user and roles

---
+++ WebStore.Domain/Entities/Identity/Role.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Entities.Identity
+{
+    public class Role : IdentityRole
+    {
+        public  const string Admin = "Admins";
+
+        public  const string User = "Users";
+
+        public string Description { get; set; }
+
+    }
+}
---
+++ WebStore.Domain/Entities/Identity/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Entities.Identity
+{
+    public class User : IdentityUser
+    {
+        public const string Admin = "Admin";
+
+        public const string DefaultADminPassword = "AdPass";
+
+        public  string Description { get; set; }
+    }
+}
