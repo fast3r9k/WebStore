1af6cfac27a2b1553d82708e2cca106a361e923d
fast3r9k <faster20092009@yandex.ru>
2020-12-21 14:24:54 +0300

    Add Cart model

--- WebStore.Domain/Entities/Cart.cs
+++ WebStore.Domain/Entities/Cart.cs
@@ -7,7 +7,7 @@ namespace WebStore.Domain.Entities
 {
     public class Cart
     {
-        public  ICollection<CartItem> Items { get; set; }
+        public ICollection<CartItem> Items { get; set; } = new List<CartItem>();
         public int ItemsCount => Items?.Sum(item => item.Quantity) ?? 0;
     }
 
