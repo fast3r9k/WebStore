d1353b3d2eac797dc6f21dc9b4fb46ec7816f1f2
fast3r9k <faster20092009@yandex.ru>
2020-12-22 13:36:12 +0300

    Login/logout to admin panel

--- WebStore/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = Role.Admin)] 
     public class CatalogController : Controller
     {
+        private readonly IProductData _ProductData;
+
+        public CatalogController(IProductData ProductData) => _ProductData = ProductData;
         public IActionResult Index()
         {
             return View();
