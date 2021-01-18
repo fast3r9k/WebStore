6983aac338bb4bbcbee66a5e98e4fe0fc24d7797
fast3r9k <faster20092009@yandex.ru>
2021-01-18 18:04:38 +0300

    Test WebApi Client

---
+++ Services/WebStore.Clients/Values/ValuesClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using WebStore.Clients.Base;
+using WebStore.Interfaces.TestApi;
+
+namespace WebStore.Clients.Values
+{
+    public class ValuesClient : BaseClient, IValuesService
+    {
+        public ValuesClient(HttpClient Client, string ServiceAddress) : base(Client, "api/values") { }
+
+        public IEnumerable<string> Get()
+        {
+            var response = Http.GetAsync(Address).Result;
+            if (response.IsSuccessStatusCode)
+                return response.Content.ReadAsAsync<IEnumerable<string>>().Result;
+            return Enumerable.Empty<string>();
+        }
+
+        public string Get(int id)
+        {
+            var response = Http.GetAsync($"{Address}/{id}").Result;
+            if (response.IsSuccessStatusCode)
+                return response.Content.ReadAsAsync<string>().Result;
+            return string.Empty;
+        }
+
+
+        public Uri Post(string value)
+        {
+            var response = Http.PostAsJsonAsync(Address, value).Result;
+            return response.EnsureSuccessStatusCode().Headers.Location;
+        }
+
+        public HttpStatusCode Update(int id, string value)
+        {
+            var response = Http.PostAsJsonAsync(Address, value).Result;
+            return response.EnsureSuccessStatusCode().StatusCode;
+        }
+
+        public HttpStatusCode Delete(int id)
+        {
+            var response = Http.DeleteAsync($"{Address}/{id}").Result;
+            return response.StatusCode;
+        }
+
+    }
+}
---
+++ Services/WebStore.Interfaces/TestApi/IValuesService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace WebStore.Interfaces.TestApi
+{
+    public  interface IValuesService
+    {
+        IEnumerable<string> Get();
+
+        string Get(int id);
+
+        Uri Post(string value);
+
+        HttpStatusCode Update(int id, string value);
+        HttpStatusCode Delete(int id);
+
+    }
+}
