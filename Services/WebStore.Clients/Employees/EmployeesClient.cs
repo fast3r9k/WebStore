1c6f8643f023b9a1fc0d75c5495fe818ba1a475f
fast3r9k <faster20092009@yandex.ru>
2021-01-22 17:54:38 +0300

    employee managment client

---
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using WebStore.Clients.Base;
+using WebStore.Domain.Models;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Employees
+{
+    public class EmployeesClient :BaseClient, IEmployeesData
+    {
+        private readonly ILogger<EmployeesClient> _Logger;
+
+        public EmployeesClient(IConfiguration Configuration, ILogger<EmployeesClient> Logger) 
+            : base(Configuration, "api/employees") => _Logger = Logger;
+
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(Address);
+
+        public Employee Get(int id) => Get<Employee>($"{Address}/{id}");
+
+        public int Add(Employee employee) => Post(Address, employee).Content.ReadAsAsync<int>().Result;
+
+        public void Update(Employee employee) => Put(Address, employee);
+
+        public bool Delete(int id) => Delete($"{Address}/{id}").IsSuccessStatusCode;
+    }
+}
