2ddc8b8110967151262564a36b6518231c6b7e77
fast3r9k <faster20092009@yandex.ru>
2021-01-26 18:23:32 +0300

    Add users client

---
+++ Services/WebStore.Clients/Identity/RolesClient.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Interfaces;
+
+namespace WebStore.Clients.Identity
+{
+    public class RolesClient : BaseClient
+    {
+        public RolesClient(IConfiguration Configuration) : base(Configuration,WebApi.Identity.Role)
+        {
+        }
+    }
+}
---
+++ Services/WebStore.Clients/Identity/UsersClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Interfaces;
+
+namespace WebStore.Clients.Identity
+{
+    public class UsersClient : BaseClient
+    {
+        public UsersClient(IConfiguration Configuration) : base(Configuration, WebApi.Identity.User)
+        {
+        }
+    }
+}
