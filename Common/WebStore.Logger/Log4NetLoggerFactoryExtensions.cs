b7f1031e1ed82f737d1eee5ae79e30dbd5f61804
fast3r9k <faster20092009@yandex.ru>
2021-01-30 20:07:11 +0300

    Add extension for adding logger

---
+++ Common/WebStore.Logger/Log4NetLoggerFactoryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    public static class Log4NetLoggerFactoryExtensions
+    {
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory Factory, string ConfigurationFile = "log4Net.config")
+        {
+            if(ConfigurationFile is null) throw new ArgumentNullException(nameof(ConfigurationFile));
+
+            if (!Path.IsPathRooted(ConfigurationFile))
+            {
+                var assembly = Assembly.GetEntryAssembly() ?? throw new InvalidOperationException("Failed to determine the library includes the entry point ");
+                var dir = Path.GetDirectoryName(assembly.Location) ?? throw new InvalidOperationException("Empty link received");
+                ConfigurationFile = Path.Combine(dir, ConfigurationFile);
+            }
+            Factory.AddProvider(new Log4NetLoggerProvider(ConfigurationFile));
+            return Factory;
+        }
+    }
+}
