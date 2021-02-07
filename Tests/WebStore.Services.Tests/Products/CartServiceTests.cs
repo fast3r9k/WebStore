3e3cd105d12e3d3399eb8c67ba26c20da1882186
fast3r9k <faster20092009@yandex.ru>
2021-02-07 21:45:47 +0300

    Cart service refactoring

---
+++ Tests/WebStore.Services.Tests/Products/CartServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebStore.Domain;
+using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+
+using Assert = Xunit.Assert;
+
+namespace WebStore.Services.Tests.Products
+{
+    [TestClass]
+    public class CartServiceTests
+    {
+        private Cart _Cart;
+        private Mock<IProductData> _ProductDataMock;
+       // private Mock<ICartStore> _CartStoreMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _Cart = new Cart
+            {
+                Items = new List<CartItem>
+                {
+                    new() { ProductId = 1, Quantity = 1 },
+                    new() { ProductId = 2, Quantity = 3 },
+                }
+            };
+
+            _ProductDataMock = new Mock<IProductData>();
+            _ProductDataMock
+               .Setup(c => c.GetProducts(It.IsAny<ProductFilter>()))
+               .Returns(new ProductDTO[]
+                {
+                    new(1, "Product 1", 0, 1.1m, "Product1.png", new BrandDTO(1, "Brand 1", 1, 1), new SectionDTO(1, "Section 1", 1, null, 1)),
+                    new(2, "Product 2", 1, 2.2m, "Product2.png", new BrandDTO(2, "Brand 2", 2, 1), new SectionDTO(2, "Section 2", 2, null, 1)),
+                });
+
+        }
+
+        [TestMethod]
+        public void CartClassItemsCountReturnsCorrectQuantity()
+        {
+            var cart = _Cart;
+            const int expected_Count = 4;
+
+            var actual_Count = cart.ItemsCount;
+
+            Assert.Equal(expected_Count, actual_Count);
+        }
+
+        [TestMethod]
+        public void CartViewModelReturnsCorrectItemsCount()
+        {
+            var cart_view_model = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+            const int expected_count = 4;
+
+            var actual_count = cart_view_model.ItemsCount;
+
+            Assert.Equal(expected_count, actual_count);
+        }
+    }
+}
