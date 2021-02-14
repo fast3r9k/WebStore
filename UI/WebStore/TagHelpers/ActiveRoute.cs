95c13a24a22bfc8ce10d533fa0711eefa7d5bc73
fast3r9k <faster20092009@yandex.ru>
2021-02-14 14:26:20 +0300

    Tag helper

---
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore.TagHelpers
+{
+
+    [HtmlTargetElement(Attributes = AttributeName)]
+    public class ActiveRoute : TagHelper
+    {
+        
+        private const string AttributeName = "is-active-route";
+        
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-action")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public Dictionary<string, string> RouteValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+
+        [ViewContext,HtmlAttributeNotBound]
+        public ViewContext viewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+            output.Attributes.RemoveAll(AttributeName); 
+        }
+    }
+}
