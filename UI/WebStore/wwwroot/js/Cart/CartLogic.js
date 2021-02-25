a2242204144215e1f2f9aaed3a443453aa82c9ae
fast3r9k <faster20092009@yandex.ru>
2021-02-25 14:04:45 +0300

    add row data to html

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -7,8 +7,17 @@
     init: function(properties) {
         $.extend(Cart._properties, properties);
 
+        Cart.initEvents();
+
+    },
+
+    initEvents: function () {
+
         $(".add-to-cart").click(Cart.addToCart);
 
+        $(".cart-quantity-up").click(Cart.incrementItem);
+        $(".cart-quantity-down").click(Cart.decrementItem);
+        $(".cart-quantity-delete").click(Cart.removeItem);
     },
 
     addToCart: function(event) {
@@ -40,5 +49,47 @@
                 container.html(cartHtml);
             })
             .fail(function () { console.log("refreshCartView fail") });
-    }
+    },
+
+    incrementItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id");
+
+        //$.get(Cart._properties.addToCartLink + "/" + id)
+        //    .done(function () {
+        //        Cart.showToolTip(button);
+        //        Cart.refreshCartView();
+        //    })
+        //    .fail(function () { console.log("addToCart fail") });
+    },
+
+    decrementItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id");
+
+        //$.get(Cart._properties.addToCartLink + "/" + id)
+        //    .done(function () {
+        //        Cart.showToolTip(button);
+        //        Cart.refreshCartView();
+        //    })
+        //    .fail(function () { console.log("addToCart fail") });
+    },
+
+    removeItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id");
+
+        //$.get(Cart._properties.addToCartLink + "/" + id)
+        //    .done(function () {
+        //        Cart.showToolTip(button);
+        //        Cart.refreshCartView();
+        //    })
+        //    .fail(function () { console.log("addToCart fail") });
+    },
 }
